b1ee8357e6c5a868350e56a124548fad7bbb0b27
zhao.song <song0586@126.com>
2020-04-22 15:33:35 +0800

    feat: 本地仓库工程添加到git

---
+++ lessons-in-wangwenjun/concurreny-in-one/src/main/java/com/concurreny/demo/DemoTest.java
@@ -0,0 +1,15 @@
+package com.concurreny.demo;
+
+/**
+ * Description: TODO <BR>
+ *
+ * @Author: zhao.song
+ * @Date: 2020/4/22 15:26
+ * @Version: 1.0
+ */
+public class DemoTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
---
+++ lessons-in-wangwenjun/concurreny-in-one/src/test/java/com/concurreny/ConcurrenyInOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurreny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrenyInOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
