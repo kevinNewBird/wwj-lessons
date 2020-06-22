3cef87a4950b7df17981e6906f441f6e2db12e6f
zhao.song <song0586@126.com>
2020-06-22 10:44:23 +0800

    feat(): 多线程第二阶段,初始化提交

---
+++ lessons-in-wangwenjun/concurrency-in-two/src/main/java/com/Con2SpringApplication.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: TODO <BR>
+ *
+ * @author: zhao.song
+ * @date: 2020/6/22 10:40
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class Con2SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Con2SpringApplication.class, args);
+    }
+}
