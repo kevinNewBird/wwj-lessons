c61083a80895ae49aff0dbc345dc2a50efa22769
zhao.song <song0586@126.com>
2020-08-26 16:36:38 +0800

    Revert "feat: idea版本升级备份"
    This reverts commit 869ca16e

--- lessons-in-wangwenjun/concurrency-in-two/src/main/java/com/Con2SpringApplication.java
+++ lessons-in-wangwenjun/concurrency-in-two/src/main/java/com/Con2SpringApplication.java
@@ -4,17 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Description:  <BR>
+ * Description: TODO <BR>
  *
  * @author: zhao.song
  * @date: 2020/6/22 10:40
  * @version: 1.0
  */
-@SpringBootApplication(scanBasePackages = {"com.concurrency2","com.classloader"})
+@SpringBootApplication
 public class Con2SpringApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(Con2SpringApplication.class, args);
     }
 }
--- lessons-in-wangwenjun/concurreny-in-one/src/main/java/com/concurreny/base/c10_booleanlock/BooleanLock.java
+++ lessons-in-wangwenjun/concurreny-in-one/src/main/java/com/concurreny/base/c10_booleanlock/BooleanLock.java
@@ -82,7 +82,6 @@ public class BooleanLock implements Lock {
                 throw new TimeOutException("Time out");
             }
             oLockedThreadContainer.add(Thread.currentThread());
-
             this.wait(mills);
 //            System.out.println("->"+System.currentTimeMillis());
             hasRemaining =endTime- System.currentTimeMillis();
